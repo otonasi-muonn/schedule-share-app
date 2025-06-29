7c0662989741810ffed30fc06ecffd5b604858e6
otonasi-muonn <nayudoutianzhong@gmail.com>
2025-06-29 14:01:14 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/schedule_share_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.schedule_share_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+// 必要なファイルをインポートします
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // さっき作成されたファイルです！
+
+// main()関数を書き換えて、Firebaseの初期化処理を追加します
+void main() async {
+  // FlutterアプリでFirebaseを動かすためのおまじないです
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Schedule Share App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // 動作確認のため、画面中央にメッセージを表示します
+      home: const Scaffold(
+        body: Center(
+          child: Text('Firebase接続完了！'),
+        ),
+      ),
+    );
+  }
+}
